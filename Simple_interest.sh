63260c1160f7b05e3e268b0a4eb2911e21082611
Amanti19 <139166963+Amanti19@users.noreply.github.com>
2023-07-21 11:44:12 +0300

    Create Simple_interest.sh
    simple_interest

---
+++ Simple_interest.sh
@@ -0,0 +1,7 @@
+Calculate the calculate interst give principle, annual reat of interest and time preiod in a year.
+Input
+p,principal amount
+t,time perod in ayear
+r, annual rate of interest
+output
+simple interest = p*t*r
